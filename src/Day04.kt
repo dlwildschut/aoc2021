d33ed248dba201b6f5b08321ff9b3368346fa21b
Daniel Wildschut <dlwildschut@gmail.com>
2021-12-23 12:43:55 +0100

    Solve day 4

---
+++ src/Day04.kt
@@ -0,0 +1,76 @@
+fun main() {
+
+    fun parse(input: List<String>): Pair<List<Int>, Set<BingoBoard>> {
+        val bingoNumbers = input[0].split(",").map { it.toInt() }
+
+        val boards =
+            input.asSequence()
+                .drop(1)
+                .filter { it.isNotBlank() }
+                .map { it ->
+                    it.split(" ")
+                        .filter { it.isNotBlank() }
+                        .map { it.toInt() }
+                }
+                .chunked(5)
+                .map { BingoBoard(it) }
+                .toSet()
+
+        return Pair(bingoNumbers, boards)
+    }
+
+    fun findWinners(
+        boards: Set<BingoBoard>,
+        bingoNumbers: List<Int>
+    ): List<Pair<Int, BingoBoard>> {
+        val mutableBoards = boards.toMutableList()
+
+        val winners = buildList {
+            for (number in bingoNumbers) {
+                mutableBoards.replaceAll { markNumber(it, number) }
+                val winningBoards = mutableBoards.filter { it.isComplete() }
+                mutableBoards -= winningBoards.toSet()
+                winningBoards.forEach { add(Pair(it.score(number), it)) }
+            }
+        }
+        return winners
+    }
+
+    fun part1(input: List<String>): Int {
+        val (bingoNumbers, boards) = parse(input)
+
+        val winners = findWinners(boards, bingoNumbers)
+        return winners.first().first
+    }
+
+    fun part2(input: List<String>): Int {
+        val (bingoNumbers, boards) = parse(input)
+
+        val winners = findWinners(boards, bingoNumbers)
+        return winners.last().first
+    }
+
+    val testInput = readInput("Day04_test")
+    check(part1(testInput) == 4512)
+    check(part2(testInput) == 1924)
+
+    val input = readInput("Day04")
+    println(part1(input))
+    println(part2(input))
+}
+
+data class BingoBoard(val rows: List<List<Int>>)
+
+private fun markNumber(board: BingoBoard, number: Int): BingoBoard {
+    return BingoBoard(board.rows.map { row -> row.map { if (it == number) -1 else it } })
+}
+
+private fun BingoBoard.isComplete(): Boolean {
+    return this.rows.any { row -> row.all { it == -1 } }
+            || this.rows.indices.any { col -> this.rows.all { it[col] == -1 } }
+}
+
+private fun BingoBoard.score(winningNumber: Int): Int {
+    return winningNumber * rows.sumOf { row -> row.filter { it != -1 }.sum() }
+}
+
