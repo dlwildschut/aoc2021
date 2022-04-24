5a84b881d754336e2128726b1137a97e1c92cc07
Daniel Wildschut <dlwildschut@gmail.com>
2022-04-24 14:44:47 +0200

    Solve day 9 Part 2

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,22 +1,42 @@
 fun main() {
 
+    fun isLowest(points: List<Point>, it: Point) =
+        points.adjacent(it).fold(true, { result, adjacent -> result && it.height < adjacent.height })
+
     fun part1(input: List<String>): Int {
         val points = input.map { it.map { it.digitToInt() }.toIntArray() }.points()
 
         // find the locations that are lower than any of its adjacent locations
         val lowest = points.filter {
-            points.adjacent(it).fold(true, { result, adjacent -> result && it.height < adjacent.height })
+            isLowest(points, it)
         }
 
         // What is the sum of the risk levels of all low points on your heightmap
         return lowest.sumOf { it.riskLevel() }
     }
 
+
+    fun part2(input: List<String>): Int {
+        val points = input.map { it.map { it.digitToInt() }.toIntArray() }.points()
+
+        // find the locations that are lower than any of its adjacent locations
+        val lowest = points.filter {
+            isLowest(points, it)
+        }
+
+        val basins = lowest.map { points.findBasins(it) }.sortedByDescending { it.size }
+
+        // What do you get if you multiply together the sizes of the three largest basins?
+        return basins.take(3).map { it.size }.reduce { acc, i -> acc * i }
+    }
+
     val testInput = readInput("Day09_test")
     check(part1(testInput) == 15)
+    check(part2(testInput) == 1134)
 
     val input = readInput("Day09")
     println(part1(input))
+    println(part2(input))
 }
 
 private data class Point(val x: Int, val y: Int, val height: Int)
@@ -40,4 +60,16 @@ private fun List<Point>.adjacent(point: Point): List<Point> {
 
 private fun Point.riskLevel(): Int {
     return 1 + height
+}
+
+private fun List<Point>.findBasins(initial: Point): Set<Point> {
+    val visited = mutableSetOf<Point>()
+    val queue = ArrayDeque<Point>()
+    queue += initial
+    while (queue.isNotEmpty()) {
+        val current = queue.removeFirst()
+        visited += current
+        queue += adjacent(current).filter { it !in visited && it.height != 9 }
+    }
+    return visited
 }
